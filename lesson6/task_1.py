7302168b61f0740e0394301d7c33f312f7c4d845
MarinaStef <154669878+MarinaStef@users.noreply.github.com>
2024-07-14 19:11:36 +0300

    task_1.py

--- lesson6/task_1.py
+++ lesson6/task_1.py
@@ -1,18 +1,19 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-wait = WebDriverWait(driver, 40, 0.1)
+driver = webdriver.Chrome(
+service=ChromeService(ChromeDriverManager().install()))
+
+driver.implicitly_wait(5)
 
 try:
     driver.get("http://uitestingplayground.com/ajax")
     blue_button = driver.find_element(By.CSS_SELECTOR, "#ajaxButton").click()
     text_from_content = wait.until(EC.visibility_of_element_located(
         (By.CSS_SELECTOR, ".bg-success"))).text
-    sleep(2)
     print(text_from_content)
 except Exception as ex:
     print(ex)
