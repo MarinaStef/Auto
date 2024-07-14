3e15bfe05446262c02cb75ed37c7c665265f25f4
MarinaStef <154669878+MarinaStef@users.noreply.github.com>
2024-07-14 19:12:54 +0300

    task_2.py

--- lesson6/task_2.py
+++ lesson6/task_2.py
@@ -1,17 +1,17 @@
 from selenium import webdriver
 from time import sleep
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(
+service=ChromeService(ChromeDriverManager().install()))
+
+driver.implicitly_wait(5)
 
 try:
     driver.get("http://uitestingplayground.com/textinput")
     button_name = driver.find_element(
         "id", "newButtonName").send_keys("SkyPro")
-    sleep(2)
     confirm_button_name = driver.find_element("id", "updatingButton").click()
-    sleep(2)
     new_button_name = driver.find_element("id", "updatingButton").text
-    sleep(2)
     print(new_button_name)
 except Exception as ex:
     print(ex)
