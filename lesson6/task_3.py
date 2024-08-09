8825514595d720ccd68244027a4fcf3da8b355f1
MarinaStef <154669878+MarinaStef@users.noreply.github.com>
2024-08-09 17:14:18 +0300

    Update task_3.py

--- lesson6/task_3.py
+++ lesson6/task_3.py
@@ -1,16 +1,25 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
-wait = WebDriverWait(driver, 40, 0.1)
-driver.implicitly_wait(5)
+driver = webdriver.Chrome(
+    service=ChromeService(ChromeDriverManager().install()))
+driver.maximize_window()
 
-try:
-    driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
-    wait.until(EC.text_to_be_present_in_element((By.ID, "text"), 'Done!'))
-    get_attribute = driver.find_element(By.ID, "award").get_attribute("src")
-    print(get_attribute)
-except Exception as ex:
-    print(ex)
+driver.get(
+    "https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
+
+waiter = WebDriverWait(driver, 10)
+
+waiter.until(EC.text_to_be_present_in_element(
+    (By.CSS_SELECTOR, 'p#text'), "Done!"))
+
+img_list = driver.find_elements(By.CSS_SELECTOR, "div#image-container > img")
+find_element = img_list[2].get_attribute("src")
+
+print(find_element)
+
+driver.quit()
